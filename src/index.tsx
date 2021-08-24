00e0b91817690fd3568dafc0d9307ae7f5107448
bastiengrignon <basgrignon@gmail.com>
2021-08-24 11:37:10 +0200

    Add missing css import

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import CookieNotice from './components/index'
 import GoogleAnalytics from './components/services/google-analytics'
 import Hubspot from './components/services/hubspot'
+import './styles.module.css'
 
 export { CookieNotice, GoogleAnalytics, Hubspot }
