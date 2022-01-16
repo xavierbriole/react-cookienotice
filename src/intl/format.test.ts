cb26af91f0d9458353859c802f7f01f7c010574d
Xavier Briole <xbriole@icloud.com>
2022-01-16 19:43:34 +0100

    Add intl tests

---
+++ src/intl/format.test.ts
@@ -0,0 +1,23 @@
+import { formatMessage } from './format'
+
+describe('formatMessage', () => {
+  it('should return a formatted message', () => {
+    const mockedConsoleError = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {})
+
+    expect(formatMessage('message')).toBe('message')
+    expect(mockedConsoleError).toHaveBeenCalledWith(
+      '[intl] no message found for id "message"',
+    )
+  })
+
+  it('should return overriden message', () => {
+    const mockedConsoleError = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {})
+
+    expect(formatMessage('message', 'override')).toBe('override')
+    expect(mockedConsoleError).not.toHaveBeenCalled()
+  })
+})
