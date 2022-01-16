e91dc633b41e3eb717f454c07d372adfb563132c
Xavier Briole <xbriole@icloud.com>
2022-01-16 16:58:46 +0100

    Add Buttons tests

---
+++ src/components/buttons/index.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Buttons from './index'
+
+describe('Buttons', () => {
+  describe('with open in new tab false', () => {
+    it('should render', () => {
+      const { container } = render(
+        <Buttons
+          acceptButtonLabel='acceptButtonLabel'
+          handleAcceptButtonClick={() => {}}
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={false}
+        />,
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+  })
+
+  describe('with open in new tab true', () => {
+    it('should render', () => {
+      const { container } = render(
+        <Buttons
+          acceptButtonLabel='acceptButtonLabel'
+          handleAcceptButtonClick={() => {}}
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={true}
+        />,
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+  })
+})
---
+++ src/components/buttons/read-more.test.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import ReadMoreButton from './read-more'
+
+describe('ReadMoreButton', () => {
+  describe('with open in new tab false', () => {
+    it('should render', () => {
+      const { container } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={false}
+        />,
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should display read more button label', () => {
+      const { getByText } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={false}
+        />,
+      )
+
+      expect(getByText('readMoreButtonLabel')).toBeInTheDocument()
+    })
+
+    it('should have right attributes', () => {
+      const { getByText } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={false}
+        />,
+      )
+
+      const readMoreButton = getByText('readMoreButtonLabel')
+
+      readMoreButton.click()
+
+      expect(readMoreButton.getAttribute('target')).toBe('_self')
+      expect(readMoreButton.getAttribute('rel')).toBe(null)
+    })
+  })
+
+  describe('with open in new tab true', () => {
+    it('should render', () => {
+      const { container } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={true}
+        />,
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should display read more button label', () => {
+      const { getByText } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={true}
+        />,
+      )
+
+      expect(getByText('readMoreButtonLabel')).toBeInTheDocument()
+    })
+
+    it('should have right attributes', () => {
+      const { getByText } = render(
+        <ReadMoreButton
+          readMoreButtonLabel='readMoreButtonLabel'
+          readMoreButtonLink='readMoreButtonLink'
+          readMoreButtonOpenInNewTab={true}
+        />,
+      )
+
+      const readMoreButton = getByText('readMoreButtonLabel')
+
+      readMoreButton.click()
+
+      expect(readMoreButton.getAttribute('target')).toBe('_blank')
+      expect(readMoreButton.getAttribute('rel')).toBe('noreferrer')
+    })
+  })
+})
