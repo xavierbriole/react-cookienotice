13b2a37409130c8a01b1d0335366d175fbac4abb
Xavier Briole <xbriole@icloud.com>
2022-08-03 12:22:09 +0200

    classnames: add some tests

--- src/helpers/classnames.test.ts
+++ src/helpers/classnames.test.ts
@@ -6,6 +6,14 @@ describe('classnames', () => {
   })
 
   describe('should compute classnames', () => {
+    it('with null', () => {
+      expect(classnames(null)).toBe('')
+    })
+
+    it('with no arguments', () => {
+      expect(classnames()).toBe('')
+    })
+
     it('with strings', () => {
       expect(classnames('foo', 'bar')).toBe('foo bar')
       expect(classnames('foo', 'bar', 'baz')).toBe('foo bar baz')
@@ -33,10 +41,27 @@ describe('classnames', () => {
 
     it('with arrays and strings', () => {
       expect(classnames(['foo', 'bar'], 'baz', 'qux')).toBe('foo bar baz qux')
+      expect(classnames(['foo', 'bar'], 'baz', ['qux', 'quux'])).toBe(
+        'foo bar baz qux quux',
+      )
     })
 
     it('with arrays and objects', () => {
       expect(classnames(['foo', 'bar'], { baz: true, qux: true })).toBe(
+        'foo bar baz qux',
+      )
+      expect(classnames(['foo', 'bar'], { baz: true, qux: false })).toBe(
+        'foo bar baz',
+      )
+      expect(classnames(['foo', 'bar'], { baz: true, qux: true }, 'quux')).toBe(
+        'foo bar baz qux quux',
+      )
+    })
+
+    it('with string and objects', () => {
+      expect(classnames('foo', { bar: true, baz: true })).toBe('foo bar baz')
+      expect(classnames('foo', { bar: true, baz: false })).toBe('foo bar')
+      expect(classnames('foo', { bar: true, baz: true }, 'qux')).toBe(
         'foo bar baz qux',
       )
     })
