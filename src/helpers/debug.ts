19f7deadf7759a3ada1fea206df2156725b2ee8f
Xavier Briole <xbriole@icloud.com>
2023-04-09 02:31:26 +0200

    Version 6

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,5 +1,6 @@
 export const err = (message: string): void => {
   if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
     console.error(`[react-cookienotice] ${message}`)
   }
 }
