4b35bc694453e359bae6598ad9bd03b1943e905b
Xavier Briole <xbriole@icloud.com>
2023-05-18 21:29:54 +0200

    Support de, es, it, ko & zh languages

--- src/intl/messages/index.ts
+++ src/intl/messages/index.ts
@@ -1,9 +1,19 @@
+import de from './de.json'
 import en from './en.json'
+import es from './es.json'
 import fr from './fr.json'
+import it from './it.json'
+import ko from './ko.json'
 import oc from './oc.json'
+import zh from './zh.json'
 
 export default {
+  de,
   en,
+  es,
   fr,
+  it,
+  ko,
   oc,
+  zh,
 } as Record<string, Record<string, string>>
